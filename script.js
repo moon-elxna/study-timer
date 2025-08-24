d6d28a143bb7af6942103fcc2282ab97a36df5f9
mooon-elxna <kreinelina@gmail.com>
2025-08-24 22:06:26 +0200

    swichting overlay

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+//swichting overlay
+function overlayClock(){
+    document.getElementById("timerOverlay").style.display = "none";
+    document.getElementById("stopwatchOverlay").style.display = "none";
+    document.getElementById("clockOverlay").style.display = "block";
+}
 
+function overlayTimer(){
+    document.getElementById("timerOverlay").style.display = "block";
+    document.getElementById("stopwatchOverlay").style.display = "none";
+    document.getElementById("clockOverlay").style.display = "none";
+}
+
+
+function overlayStopwatch(){
+    document.getElementById("timerOverlay").style.display = "none";
+    document.getElementById("stopwatchOverlay").style.display = "block";
+    document.getElementById("clockOverlay").style.display = "none";
+}
