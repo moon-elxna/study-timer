e1c79b9d7103a174ea3085fc20e29c28602089ab
moon-elxna <kreinelina@gmail.com>
2026-03-07 01:29:22 +0100

    edits

--- script.js
+++ script.js
@@ -1,70 +1,111 @@
 //---main---
-let stopwatchID;
-let elapsedTime;
-let timerID;
+const modes = ["clock", "timer", "stopwatch"]; 
+const mainpage = modes[0];
+const intervals = {
+    stopwatch: null,
+    timer: null
+}
+/*let elapsedTime;
 let remainingTime;
-let duration;
-useLocalStorage();
-setClock();
+let duration;*/
+use_localstorage()
+set_clock();
+
 
 //---EventListeners---
+for(let i = 0; i < modes.length; i++){
+    document.getElementById(modes[i]).addEventListener("click", function(){overlay_page(modes[i])});  
+}
 
 //---functions---
-// using local storage
-function useLocalStorage() {
-
-    if (localStorage.getItem("menu") == "timer") {
-        overlayTimer();
+function use_localstorage() {
+    let mode = localStorage.getItem("menu");
+    if(mode != null){
+        overlay_page(mode);
     }
-    else if (localStorage.getItem("menu") == "stopwatch") {
-        overlayStopwatch();
+    else{
+        overlay_page(mainpage);
     }
-    else {
-        overlayClock();
+    document.getElementById("sessions").innerHTML = localStorage.getItem("sessions");//to fix
+}
 
+function overlay_page(mode){
+    let index = []; let j = 0;
+    for(let i= 0; i< modes.length; i++){
+        if(modes[i] != mode){
+            index[j] = i; j++ 
+        }
     }
-
-    document.getElementById("sessions").innerHTML = localStorage.getItem("sessions");
+    document.getElementById(mode +"_overlay").style.display = "block";
+    for(let i = 0; i < index.length; i++){
+        document.getElementById(modes[index[i]] +"_overlay").style.display = "none";
+    }
+    localStorage.setItem("menu", mode)
 }
 
 function restart() {
     localStorage.removeItem("menu");
     localStorage.removeItem("sessions");
     document.getElementById("sessions").innerHTML = " ";
-    overlayClock();
+    overlay_page(mainpage);
 }
 
+function set_clock() {
+    setInterval(function () {
+        document.getElementById("time").innerHTML = unix_to_time((Date.now() / 1000).toFixed(0), "clock");
+    }, 10);;
 
-// timer, stopwatch and clock
-function startPause(mode) {
+    setInterval(function () {
+        document.getElementById("date").innerHTML = unix_to_date((Date.now() / 1000).toFixed(0));
+    }, 10);;
+}
 
-    if (mode === "stopwatch") {
-        const startPause = document.getElementById("startPauseStopwatch").innerHTML;
+function unix_to_date(timestamp) {
+    const n = new Date(timestamp * 1000); // convert sec to ms
+    return (String(n.getDate()).padStart(2, "0")) + "." + (String(n.getMonth() + 1).padStart(2, "0")) + "." + (String(n.getFullYear()));
+}
 
-        if (startPause === "Start") {
-            const startTime = Date.now();
-            stopwatchID = setInterval(function () {
-                elapsedTime = (Date.now() - (startTime));
-                document.getElementById("clockStopwatch").innerHTML = unixToTime((elapsedTime / 1000).toFixed(0));
-            }, 100);
-            document.getElementById("startPauseStopwatch").innerHTML = "Pause";
-
-        }
-        else if (startPause === "Pause") {
-            clearInterval(stopwatchID);
-            document.getElementById("startPauseStopwatch").innerHTML = "Continue";
-        }
-        else if (startPause === "Continue") {
-            const startTime = Date.now() - elapsedTime;
-            stopwatchID = setInterval(function () {
-                elapsedTime = (Date.now() - (startTime));
-                document.getElementById("clockStopwatch").innerHTML = unixToTime((elapsedTime / 1000).toFixed(0));
-            }, 100);
-            document.getElementById("startPauseStopwatch").innerHTML = "Pause";
-        }
+function unix_to_time(timestamp, mode) {
+    if (mode === "clock") {
+        const n = new Date(timestamp * 1000); // convert seconds → ms
+        return (String(n.getHours()).padStart(2, "0")) + ":" + (String(n.getMinutes()).padStart(2, "0")) + ":" + (String(n.getSeconds()).padStart(2, "0"));
     }
+    else {
+        const totalSeconds = Math.floor(timestamp);
+        return (String(Math.floor(totalSeconds / 3600)).padStart(2, "0")) + ":" + (String(Math.floor((totalSeconds % 3600) / 60)).padStart(2, "0")) + ":" + (String(totalSeconds % 60).padStart(2, "0"));
+    }
+}
 
-    else if (mode === "timer") {
+function time_to_unix(hours, minutes, seconds) {
+    return (hours * 3600000) + (minutes * 60000) + (seconds * 1000); //conversion into ms
+}
+
+function start_pause_stopwatch() {
+    const startPause = document.getElementById("startPauseStopwatch").innerHTML;
+    if (startPause === "Start") {
+        const startTime = Date.now();
+        stopwatch_id = setInterval(function () {
+            elapsedTime = (Date.now() - (startTime));
+            document.getElementById("clockStopwatch").innerHTML = unix_to_time((elapsedTime / 1000).toFixed(0));
+        }, 100);
+        document.getElementById("startPauseStopwatch").innerHTML = "Pause";
+
+    }
+    else if (startPause === "Pause") {
+        clearInterval(stopwatch_id);
+        document.getElementById("startPauseStopwatch").innerHTML = "Continue";
+    }
+    else if (startPause === "Continue") {
+        const startTime = Date.now() - elapsedTime;
+        stopwatch_id = setInterval(function () {
+            elapsedTime = (Date.now() - (startTime));
+            document.getElementById("clockStopwatch").innerHTML = unix_to_time((elapsedTime / 1000).toFixed(0));
+        }, 100);
+            document.getElementById("startPauseStopwatch").innerHTML = "Pause";
+    }
+}
+
+function start_pause_timer() {
 
         if (
             ((document.getElementById("hourTimer").value === "") && (document.getElementById("minuteTimer").value === "") && (document.getElementById("secondTimer").value === ""))
@@ -76,7 +117,7 @@ function startPause(mode) {
             const minutes = parseInt(document.getElementById("minuteTimer").value) || 0;
             const seconds = parseInt(document.getElementById("secondTimer").value) || 0;
 
-            duration = timeToUnix(hours, minutes, seconds);
+            duration = time_to_unix(hours, minutes, seconds);
 
             const startPause = document.getElementById("startPauseTimer").innerHTML;
             if (startPause === "Start") {
@@ -85,40 +126,42 @@ function startPause(mode) {
                 document.getElementById("minuteTimer").value = "";
                 document.getElementById("secondTimer").value = "";
                 const targetTime = Date.now() + duration;
-                timerID = setInterval(function () {
+                timer_id = setInterval(function () {
                     remainingTime = (targetTime) - Date.now();
-                    document.getElementById("clockTimer").innerHTML = unixToTime((remainingTime / 1000).toFixed(0));
+                    document.getElementById("clockTimer").innerHTML = unix_to_time((remainingTime / 1000).toFixed(0));
                 }, 100);
                 document.getElementById("startPauseTimer").innerHTML = "Pause";
             }
 
             else if (startPause === "Pause") {
-                clearInterval(timerID);
+                clearInterval(timer_id);
                 document.getElementById("startPauseTimer").innerHTML = "Continue";
             }
 
             else if (startPause === "Continue") {
                 const targetTime = remainingTime + Date.now();
-                timerID = setInterval(function () {
+                timer_id = setInterval(function () {
                     remainingTime = (targetTime) - Date.now();
-                    document.getElementById("clockTimer").innerHTML = unixToTime((remainingTime / 1000).toFixed(0));
+                    document.getElementById("clockTimer").innerHTML = unix_to_time((remainingTime / 1000).toFixed(0));
                 }, 100);
                 document.getElementById("startPauseTimer").innerHTML = "Pause";
             }
 
         }
-    }
-
 }
 
 function reset(mode) {
+    clearInterval(intervals[mode]);
+    document.getElementById("clock_" + mode).innerHTML = "00:00:00";
+    document.getElementById("startPauseStopwatch").innerHTML = "Start"
+
     if (mode === "stopwatch") {
-        clearInterval(stopwatchID);
-        document.getElementById("clockStopwatch").innerHTML = "00:00:00";
-        document.getElementById("startPauseStopwatch").innerHTML = "Start"
+        
+        
+        
     }
     else if (mode === "timer") {
-        clearInterval(timerID);
+        clearInterval(timer_id);
         document.getElementById("clockTimer").innerHTML = "00:00:00";
         document.getElementById("startPauseTimer").innerHTML = "Start"
     }
@@ -129,86 +172,14 @@ function save(mode) {
     const newItem = document.createElement("li");
 
     if (mode === "timer") {
-        newItem.textContent = unixToTime((duration / 1000) || 0);
+        newItem.textContent = unix_to_time((duration / 1000) || 0);
         list.appendChild(newItem);
     }
     else if (mode === "stopwatch") {
         reset(mode);
-        newItem.textContent = unixToTime(elapsedTime / 1000);
+        newItem.textContent = unix_to_time(elapsedTime / 1000);
         list.appendChild(newItem);
     }
 
     localStorage.setItem("sessions", list)
-}
-
-
-function setClock() {
-    setInterval(function () {
-        document.getElementById("time").innerHTML = unixToTime((Date.now() / 1000).toFixed(0), "clock");
-    }, 10);;
-
-    setInterval(function () {
-        document.getElementById("date").innerHTML = unixToDate((Date.now() / 1000).toFixed(0));
-    }, 10);;
-}
-
-
-//overlay pages
-function overlayClock() {
-    document.getElementById("timerOverlay").style.display = "none";
-    document.getElementById("stopwatchOverlay").style.display = "none";
-    document.getElementById("clockOverlay").style.display = "block";
-    localStorage.setItem("menu", "clock")
-}
-
-function overlayTimer() {
-    document.getElementById("timerOverlay").style.display = "block";
-    document.getElementById("stopwatchOverlay").style.display = "none";
-    document.getElementById("clockOverlay").style.display = "none";
-    localStorage.setItem("menu", "timer")
-}
-
-function overlayStopwatch() {
-    document.getElementById("timerOverlay").style.display = "none";
-    document.getElementById("stopwatchOverlay").style.display = "block";
-    document.getElementById("clockOverlay").style.display = "none";
-    localStorage.setItem("menu", "stopwatch")
-}
-
-
-// time and date conversion
-function unixToDate(timestamp) {
-    const n = new Date(timestamp * 1000); // convert seconds → ms
-    const day = String(n.getDate()).padStart(2, "0");
-    const month = String(n.getMonth() + 1).padStart(2, "0"); // months start at 0
-    const year = String(n.getFullYear());
-    return day + "." + month + "." + year;
-
-}
-
-function unixToTime(timestamp, mode) {
-    if (mode === "clock") {
-        const n = new Date(timestamp * 1000); // convert seconds → ms
-        const hours = String(n.getHours()).padStart(2, "0");
-        const minutes = String(n.getMinutes()).padStart(2, "0");
-        const seconds = String(n.getSeconds()).padStart(2, "0");
-        return hours + ":" + minutes + ":" + seconds;
-
-    }
-    else {
-        const totalSeconds = Math.floor(timestamp);
-        const hours = String(Math.floor(totalSeconds / 3600)).padStart(2, "0");
-        const minutes = String(Math.floor((totalSeconds % 3600) / 60)).padStart(2, "0");
-        const seconds = String(totalSeconds % 60).padStart(2, "0");
-        return hours + ":" + minutes + ":" + seconds;
-    }
-}
-
-function timeToUnix(hours, minutes, seconds) {
-    //conversion into ms
-    const hoursMs = hours * 3600000;
-    const minutesMs = minutes * 60000;
-    const secondsMs = seconds * 1000;
-    const timestamp = hoursMs + minutesMs + secondsMs;
-    return timestamp;
 }
