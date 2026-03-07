1f6494a5007c90702e01b6f581546b538788de22
moon-elxna <kreinelina@gmail.com>
2026-03-07 03:41:01 +0100

    edits

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 //---main---
 const modes = ["clock", "timer", "stopwatch"];
+const btn_states = [0, 1, 2]; //0 = start, 1 = pause, 2 = continue
 const main_page = modes[0];
-const intervals = { stopwatch: null, timer: null }
+const state = { 
+    stopwatch:{interval: null, btn_state: btn_states[0]}, 
+    timer: {interval: null, btn_state: btn_states[0]}
+}
 let elapsed_time;
 let remaining_time;
 let duration;
@@ -15,23 +19,23 @@ for (let i = 0; i < modes.length; i++) {
 
 //---functions---
 function use_local_storage() {
-    let mode = localStorage.getItem("menu");
-    if (mode != null) { overlay_page(mode); }
+    let state = localStorage.getItem("menu");
+    if (state != null) { overlay_page(state); }
     else { overlay_page(main_page); }
     document.getElementById("sessions").innerHTML = localStorage.getItem("sessions");
 }
 
-function overlay_page(mode) {
+function overlay_page(state) {
     let index = [];
     let j = 0;
     for (let i = 0; i < modes.length; i++) {
-        if (modes[i] != mode) { index[j] = i; j++ }
+        if (modes[i] != state) { index[j] = i; j++ }
     }
-    document.getElementById(mode + "_overlay").style.display = "block";
+    document.getElementById(state + "_overlay").style.display = "block";
     for (let i = 0; i < index.length; i++) {
         document.getElementById(modes[index[i]] + "_overlay").style.display = "none";
     }
-    localStorage.setItem("menu", mode)
+    localStorage.setItem("menu", state)
 }
 
 function restart() {
@@ -44,10 +48,10 @@ function restart() {
 function set_clock() {
     setInterval(function () {
         document.getElementById("time").innerHTML = unix_to_time((Date.now() / 1000).toFixed(0), "clock");
-    }, 10);
+    }, 1000);
     setInterval(function () {
         document.getElementById("date").innerHTML = unix_to_date((Date.now() / 1000).toFixed(0));
-    }, 10);
+    }, 1000);
 }
 
 function unix_to_date(timestamp) {
@@ -55,8 +59,8 @@ function unix_to_date(timestamp) {
     return (String(n.getDate()).padStart(2, "0") + "." + String(n.getMonth() + 1).padStart(2, "0") + "." + String(n.getFullYear()));
 }
 
-function unix_to_time(timestamp, mode) {
-    if (mode === "clock") {
+function unix_to_time(timestamp, state) {
+    if (state === "clock") {
         const n = new Date(timestamp * 1000);
         return (String(n.getHours()).padStart(2, "0") + ":" + String(n.getMinutes()).padStart(2, "0") + ":" + String(n.getSeconds()).padStart(2, "0"));
     }
@@ -71,8 +75,7 @@ function time_to_unix(hours, minutes, seconds) {
 }
 
 function start_pause_stopwatch() {
-    const start_pause = document.getElementById("start_pause_stopwatch").innerHTML;
-    if (start_pause === "Start") {
+    if (state.stopwatch.btn_state == 0) {
         const start_time = Date.now();
         stopwatch_id = setInterval(function () {
             elapsed_time = (Date.now() - start_time);
@@ -80,11 +83,11 @@ function start_pause_stopwatch() {
         }, 100);
         document.getElementById("start_pause_stopwatch").innerHTML = "Pause";
     }
-    else if (start_pause === "Pause") {
+    else if (state.stopwatch.btn_state == 1) {
         clearInterval(stopwatch_id);
         document.getElementById("start_pause_stopwatch").innerHTML = "Continue";
     }
-    else if (start_pause === "Continue") {
+    else if (state.stopwatch.btn_state == 2) {
         const start_time = Date.now() - elapsed_time;
         stopwatch_id = setInterval(function () {
             elapsed_time = (Date.now() - start_time);
@@ -101,8 +104,7 @@ function start_pause_timer() {
         const minutes = parseInt(document.getElementById("minute_timer").value) || 0;
         const seconds = parseInt(document.getElementById("second_timer").value) || 0;
         duration = time_to_unix(hours, minutes, seconds);
-        const start_pause = document.getElementById("start_pause_timer").innerHTML;
-        if (start_pause === "Start") {
+        if (state.timer.btn_state == 0) {
             document.getElementById("hour_timer").value = "";
             document.getElementById("minute_timer").value = "";
             document.getElementById("second_timer").value = "";
@@ -113,11 +115,11 @@ function start_pause_timer() {
             }, 100);
             document.getElementById("start_pause_timer").innerHTML = "Pause";
         }
-        else if (start_pause === "Pause") {
+        else if (state.timer.btn_state == 1) {
             clearInterval(timer_id);
             document.getElementById("start_pause_timer").innerHTML = "Continue";
         }
-        else if (start_pause === "Continue") {
+        else if (state.timer.btn_state == 2) {
             const target_time = remaining_time + Date.now();
             timer_id = setInterval(function () {
                 remaining_time = target_time - Date.now();
@@ -128,30 +130,30 @@ function start_pause_timer() {
     }
 }
 
-function reset(mode) {
-    clearInterval(intervals[mode]);
-    document.getElementById("clock_" + mode).innerHTML = "00:00:00";
+function reset(state) {
+    clearInterval(state[state]);
+    document.getElementById("clock_" + state).innerHTML = "00:00:00";
     document.getElementById("start_pause_stopwatch").innerHTML = "Start";
-    if (mode === "stopwatch") {
+    if (state === "stopwatch") {
     }
-    else if (mode === "timer") {
+    else if (state === "timer") {
         clearInterval(timer_id);
         document.getElementById("clock_timer").innerHTML = "00:00:00";
         document.getElementById("start_pause_timer").innerHTML = "Start"
     }
 }
 
-function save(mode) {
+function save(state) {
     const list = document.getElementById("sessions");
     const new_item = document.createElement("li");
-    if (mode === "timer") {
+    if (state === "timer") {
         new_item.textContent = unix_to_time((duration / 1000) || 0);
         list.appendChild(new_item);
     }
-    else if (mode === "stopwatch") {
-        reset(mode);
+    else if (state === "stopwatch") {
+        reset(state);
         new_item.textContent = unix_to_time(elapsed_time / 1000);
         list.appendChild(new_item);
     }
-    localStorage.setItem("sessions", list)
+    localStorage.setItem("sessions", list.innerHTML)
 }
